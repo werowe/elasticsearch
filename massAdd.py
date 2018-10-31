27a4dafe8d61099cd99b7dcbd8330bb993cdbb24
werowe <werowe@walkerrowe.com>
2018-10-31 15:46:40 +0100

    Update massAdd.py

--- massAdd.py
+++ massAdd.py
@@ -11,7 +11,7 @@ def assignClasses():
       classes.append(genclass())
     return classes 
       
-
+schools = ("Arizona State University", "University of South Carolina - Columbia", "University of North Carolina at Chapel Hill") 
 firstNames = ("Walker", "Stephen", "Julie", "George")
 lastNames = ("Rowe", "Shakespeare", "Mann", "Sarte")
 courses= ("math", "physics", "French", "logic")
@@ -25,6 +25,7 @@ def genclass():
 
 def genStudent():
     students={}
+    students["school"]=schools[random.randrange(0,3)]
     students["firstName"] = firstNames[random.randrange(0,3)]
     students["lastName"] = lastNames[random.randrange(0,3)]
     students["classes"] = assignClasses()
@@ -32,9 +33,9 @@ def genStudent():
     m = hashlib.sha1()
     m.update(bytes(json.dumps(students), 'utf-8'))
     id = m.hexdigest()
-    print(students)
-    response = requests.post(url + id, json=students)
-    print(response.text)
 
-for r in range(1,4):
+    response = requests.post(url + id, json=students)
+
+
+for r in range(1,40000):
     genStudent()
