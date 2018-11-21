fbf6e5b44ef400e328f948dfddaba94c9558178d
werowe <werowe@walkerrowe.com>
2018-11-21 12:07:53 +0100

    Create parentChild.py

---
+++ parentChild.py
@@ -0,0 +1,52 @@
+import requests
+import random
+import hashlib
+import json
+
+url="http://parisx:9200/universities/doc/" 
+
+def assignClasses():
+    classes=[]
+    for x in range(random.randrange(1,6)):
+      classes.append(genclass())
+    return classes 
+      
+schools = ("Arizona State University", "University of South Carolina - Columbia", "University of North Carolina at Chapel Hill") 
+firstNames = ("Walker", "Stephen", "Julie", "George")
+lastNames = ("Rowe", "Shakespeare", "Mann", "Sarte")
+courses= ("math", "physics", "French", "logic")
+
+def genclass():
+    classes={}
+    classes["name"]=courses[random.randrange(0,3)]
+    classes["grades"] = random.randrange(1,7)
+    return classes
+
+def genSchool():
+    school={}
+    school["universities_classes"] = {'name': 'universities'}
+    school["school"]=schools[random.randrange(0,3)]
+    m = hashlib.sha1()
+    m.update(bytes(json.dumps(school), 'utf-8'))
+    id = m.hexdigest()
+    print(school)
+    response = requests.post(url + id, json=school)
+    print (response.json())
+    return id
+
+def genStudent():
+    id = genSchool()
+    students={}
+    students["universities_classes"] = {'name': 'students'}
+    students["school"]=schools[random.randrange(0,3)]
+    students["firstName"] = firstNames[random.randrange(0,3)]
+    students["lastName"] = lastNames[random.randrange(0,3)]
+    students["classes"] = assignClasses()
+
+    print(students)
+    response = requests.post(url + id, json=students)
+    print (response.json())
+
+
+for r in range(1,4):
+    genStudent()
